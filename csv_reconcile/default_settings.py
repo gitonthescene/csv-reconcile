1b99565b106a4d2763331ada0938cb3c3eb281c5
Douglas Mennella <douglas.mennella@gmail.com>
2021-12-04 10:05:13 +0900

    Clean up defaults with smart detection.  Add tests to show overrides work.

--- csv_reconcile/default_settings.py
+++ csv_reconcile/default_settings.py
@@ -2,10 +2,6 @@ import logging
 
 DATABASE = 'csvreconcile.db'
 
-CSVKWARGS = dict(delimiter='\t')
-
-# CSVENCODING='utf-8-sig'
-
 LIMIT = 10  # At most 10 matches per query
 
 THRESHOLD = 30.0  # At least a 30% match
