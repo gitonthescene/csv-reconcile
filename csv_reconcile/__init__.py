5112a0cd111c4793ff70eca65fdb335abd5fd0ae
Douglas Mennella <douglas.mennella@gmail.com>
2022-04-30 14:45:54 +0900

    Automated csv file encoding detection (defensive)

--- csv_reconcile/__init__.py
+++ csv_reconcile/__init__.py
@@ -22,7 +22,7 @@ try:
 except:
     from importlib import metadata
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 #------------------------------------------------------------------
 # Implement reconciliation API
 # [[https://reconciliation-api.github.io/specs/latest/]]
